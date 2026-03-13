d3e94a2729702e77df61b66e1007eb545dec3fc8
orioncavalcante <orion.franca19@gmail.com>
2026-03-12 22:40:39 -0300

    AULA aula 12-03-2026 Computational Thinking with Python

---
+++ Computational Thinking with Python/aula 12-03-2026.py
@@ -0,0 +1,62 @@
+# Exemplo 1
+
+#V = R x I
+
+#V = tensão
+#R = resistência
+#I = corrente
+
+# Cálculo da resistencia do carregador de celular
+# Variáveis
+#v = 5V
+#I = 0.5A
+
+# Equação
+#R = V / I
+#print(f"O valor da resistência do carregados é: {R}")
+################################################################
+
+# Exemplo 2
+#V = float(input("Digite o valor de V: "))
+#I = float(input("Digite o valor de I: "))
+
+# Equação
+#R = V / I
+
+#print(f"O valor da resistência do carregador é: {R}")
+################################################################
+
+# Exemplo 3
+
+# x = float(input("Digite o valor de X: "))
+# #10x**2 * 5x + 2
+# resultado = (10*x**2) + (5*x) + 2
+
+# print(f"O resultado desta equação será: {resultado}")
+
+################################################################
+
+# Exemplo 4
+
+# C = (F - 32) / 1.8
+
+# Fahr = float(input("Digite o valor em Fahrenheit: "))
+
+# celsius = (Fahr - 32) / 1.8
+
+# print(f"O valor em Fahrenheit é: {Fahr}")
+# print(f"O valor em celsius é: {celsius}")
+
+################################################################
+
+# Ex. 5
+m = float(input("Digite o valor da massa: "))
+
+c = 3*(10**8)
+
+e = m*(c**2)
+
+print(f"O valor da energia é: {e}")
+
+################################################################
+
