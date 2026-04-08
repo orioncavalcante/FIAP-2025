06e991e7750ea3a0f24f848183f2015349af731f
orioncavalcante <orion.franca19@gmail.com>
2026-04-07 21:02:32 -0300

    Aula JS WebDevelopment 07/04/2026

---
+++ Web Development/aula_07_04_2026/script.js
@@ -0,0 +1,31 @@
+// alert("Hello World!");
+// prompt("Qual é o seu nome?");
+// confirm("Você realmente deseja sair?");
+
+// console.log("Teste");
+
+// let nome = "Orion";
+// const PI = 3.14;
+
+// console.log(typeof nome);
+
+// let nome = prompt("Qual é o seu nome?");
+// alert(`nome ${nome} !`);
+
+
+
+// ATIVIDADE
+
+// let nome = prompt("Digite seu nome");
+// let idade = prompt("Digite sua idade");
+// let profissao = prompt("Digite sua profissão");
+
+// alert(`Olá ${nome}, você tem ${idade} anos e trabalha como ${profissao}`);
+
+
+// DESAFIO
+
+let numero1 = prompt("Digite o primeiro número");
+let numero2 = prompt("Digite o segundo número");
+let soma = Number(numero1) + Number(numero2);
+alert(`O resultado de ${numero1} + ${numero2} = ${soma}`);
